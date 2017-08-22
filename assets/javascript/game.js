4e8fc24e23fb1b91d66452f2cfe730248612151c
unknown <luca.canizzo@gmail.com>
2017-08-21 21:25:39 -0400

    Added html content, testing javascript logic

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+
+// var wordList = ["f i r s t", "s e c o n d", "t h i r d", "f o u r t h", "f i f t h", "s i x t h"];
+
+// var currentWord = wordList[Math.floor(Math.random() * wordList.length)];
+// var currentWordBox = document.getElementById("current-word");
+
+//     currentWordBox.innerHTML = currentWord;
+
+// console.log(currentWord);
+// console.log(wordList);
+
+
